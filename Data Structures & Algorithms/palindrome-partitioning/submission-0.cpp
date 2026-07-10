a810e5f1c91c44d88f266b56e0798bbe45c0e614
ADITYA RAJ <aditya195raj@gmail.com>
2026-07-10 09:44:44 +0530

    Add: palindrome-partitioning - submission-0

---
+++ Data Structures & Algorithms/palindrome-partitioning/submission-0.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    bool isPal(string &s, int left, int right) {
+        while (left < right) {
+            if (s[left] != s[right])
+                return false;
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+    void solve(int start, string &s, vector<string> &curr,
+               vector<vector<string>> &ans) {
+
+        if (start == s.size()) {
+            ans.push_back(curr);
+            return;
+        }
+
+        for (int end = start; end < s.size(); end++) {
+
+            if (isPal(s, start, end)) {
+
+                curr.push_back(s.substr(start, end - start + 1));
+
+                solve(end + 1, s, curr, ans);
+
+                curr.pop_back();
+            }
+        }
+    }
+
+    vector<vector<string>> partition(string s) {
+
+        vector<vector<string>> ans;
+        vector<string> curr;
+
+        solve(0, s, curr, ans);
+
+        return ans;
+    }
+};
